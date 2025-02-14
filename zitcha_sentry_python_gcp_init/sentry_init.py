fca1ea110ae65f176c5fcee6efe6ab04e982355a
Marcus Fabriczy <marcus.fabriczy@zitcha.com>
2025-02-14 15:25:49 +1030

    SP-6556: Correct sample rate. Remove error checks
    Removed errors checks to pass the lint check, and because the SDK itself will
    throw errors.

--- zitcha_sentry_python_gcp_init/sentry_init.py
+++ zitcha_sentry_python_gcp_init/sentry_init.py
@@ -4,16 +4,9 @@ import sentry_sdk
 from sentry_sdk.integrations.gcp import GcpIntegration
 
 def init_sentry():
-    dsn_url = os.environ.get("SENTRY_DSN_URL")
-    if not dsn_url:
-        raise ValueError("The SENTRY_DSN_URL environment variable is not set.")
-
-    project_id = os.environ.get("PROJECT_ID")
-    if not project_id:
-        raise ValueError("The PROJECT_ID environment variable is not set.")
-
-    sample_rate = float(os.environ.get("SENTRY_SAMPLE_RATE", "0.1"))
-
+    dsn_url = os.environ.get("SENTRY_DSN_URL", "")
+    project_id = os.environ.get("PROJECT_ID", "")
+    sample_rate = float(os.environ.get("SENTRY_SAMPLE_RATE", "1.0"))
     
     sentry_sdk.init(
         dsn=dsn_url,
